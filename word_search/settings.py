f9926cc99ae58300774bb939192f9560687bfe8d
Eddy Ejembi <eddyejembi2018@gmail.com>
2020-09-16 15:18:12 +0100

    second commit

--- word_search/settings.py
+++ word_search/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,4 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
